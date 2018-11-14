b51cc5c953b6eed6d34789b9d1719c11323330a7
Sean Hendrix <hendoware@gmail.com>
2018-11-14 18:43:05 -0500

    toggle on

--- Scatter and Gather/ViewController.swift
+++ Scatter and Gather/ViewController.swift
@@ -16,9 +16,9 @@ class ViewController: UIViewController {
     
     var timer = Timer()
     
-    @IBOutlet weak var lambdaPic: UIImageView!
+    //@IBOutlet weak var lambdaPic: UIImageView!
     @IBOutlet weak var lambdaLogo: UIImageView!
-    
+    @IBOutlet var letters: [UILabel]!
     
     @IBOutlet weak var letterL: UILabel!
     @IBOutlet weak var letterA1: UILabel!
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-//        initAudio()
+        initAudio()
         setup()
     }
     
@@ -63,10 +63,12 @@ class ViewController: UIViewController {
     }
     
     func setup() {
-
+        
+        //view.backgroundColor = generateRandomColor()
+        
+            
         if !shouldScamble {
-//            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector(("setRandomBackgroundColor")), userInfo: nil, repeats: true)
-//            self.setRandomBackgroundColor()
+
 //            view.backgroundColor = .green
         }
         if shouldScamble {
@@ -76,10 +78,6 @@ class ViewController: UIViewController {
         }
     }
     
-//    func randomColorGenerator() -> Int{
-//        let randomColor = Int.random(in: 1..<5)
-//        return randomColor
-//    }
     func setRandomBackgroundColor() {
         let colors = [
             UIColor(red: 233/255, green: 203/255, blue: 198/255, alpha: 1),
@@ -92,20 +90,84 @@ class ViewController: UIViewController {
         self.view.backgroundColor = colors[randomColor]
     }
     
+    func generateRandomColor() -> UIColor {
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+    
     @IBAction func toggleBtn(_ sender: Any) {
+        shouldScamble.toggle()
+        saveCoordinates()
+        //view.backgroundColor = generateRandomColor()
         if shouldScamble {
-            shouldScamble = false
-            view.backgroundColor = .green
+            scramble()
         } else {
-            shouldScamble = true
-            view.backgroundColor = .yellow
+            unscramble()
+            timer.invalidate()
         }
         
     }
     
+    func scramble() {
+        timer = Timer.scheduledTimer(timeInterval: 0, target: self, selector: #selector(self.animode), userInfo: nil, repeats: true)
+    }
     
+    func unscramble() {
+        for letter in letters {
+            UIView.animate(withDuration: 2) {
+                self.lambdaLogo.alpha = 1
+                letter.transform = .identity
+                letter.backgroundColor = nil
+                letter.textColor = .black
+                self.view.backgroundColor = .white
+            }
+        }
+    }
     
+    @objc func animode() {
+        UIView.animate(withDuration: 4, animations: {
+            self.lambdaLogo.alpha = 0
+        }) { (_) in
+            UIView.animate(withDuration: 4, animations: {
+                self.lambdaLogo.alpha = 1
+            })
+        }
+        
+        for letter in letters {
+            let location = randomLocation()
+            UIView.animate(withDuration: 1) {
+                var matrix = letter.transform
+                matrix.rotated(by: CGFloat.pi / 1.5)
+                matrix.tx = location.x
+                matrix.ty = location.y
+                
+                letter.backgroundColor = self.generateRandomColor()
+                letter.textColor = self.generateRandomColor()
+                
+                letter.transform = matrix
+            }
+        }
+        view.backgroundColor = generateRandomColor()
+    }
     
+    func randomLocation() -> (x: CGFloat, y: CGFloat) {
+        return (CGFloat.random(in: -200...200), CGFloat.random(in: -300...300))
+    }
+    
+    var xCoordinates: [CGFloat] = [8, 16, 24, 36, 44, 52]
+    var yCoordinates: [CGFloat] = [8, 16, 24, 36, 44, 52]
+    
+    func saveCoordinates() {
+        var count = 0
+        for letter in letters {
+            xCoordinates[count] = letter.center.x
+            yCoordinates[count] = letter.center.y
+            count += 1
+        }
+    }
     
 }
 
