39b36e8877111fb2e5cf02afb2a508e3c382b2cd
Sean Hendrix <hendoware@gmail.com>
2018-11-14 16:50:16 -0500

    first commit

--- Scatter and Gather/ViewController.swift
+++ Scatter and Gather/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var musicPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initAudio()
     }
-
-
+    
+    func initAudio() {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: url)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
+    }
+    
+    @IBAction func musicBtn(_ sender: UIButton) {
+        print("sorry loop = -1")
+//        if musicPlayer.isPlaying {
+//
+//            musicPlayer.pause()
+//            sender.alpha = 0.2
+//        } else{
+//            musicPlayer.play()
+//            sender.alpha = 1.0
+//        }
+    }
+    
+    
 }
 
