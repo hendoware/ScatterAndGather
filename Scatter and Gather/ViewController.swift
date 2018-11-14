75958a51605a9fca6eacd9fcbaac1767fd1fefc4
Sean Hendrix <hendoware@gmail.com>
2018-11-14 16:08:19 -0500

    Initial Commit

---
+++ Scatter and Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Sean Hendrix on 11/14/18.
+//  Copyright © 2018 Sean Hendrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
