037064a7067bf03ddeb9660543088bfadabbb1c9
Sean Hendrix <hendoware@gmail.com>
2018-11-15 10:48:14 -0500

    clean

--- Scatter and Gather/ViewController.swift
+++ Scatter and Gather/ViewController.swift
@@ -63,8 +63,6 @@ class ViewController: UIViewController {
     
     func setup() {
         
-        //view.backgroundColor = generateRandomColor()
-        
             
         if !shouldScamble {
 
@@ -77,17 +75,6 @@ class ViewController: UIViewController {
         }
     }
     
-    func setRandomBackgroundColor() {
-        let colors = [
-            UIColor(red: 233/255, green: 203/255, blue: 198/255, alpha: 1),
-            UIColor(red: 38/255, green: 188/255, blue: 192/255, alpha: 1),
-            UIColor(red: 253/255, green: 221/255, blue: 164/255, alpha: 1),
-            UIColor(red: 235/255, green: 154/255, blue: 171/255, alpha: 1),
-            UIColor(red: 87/255, green: 141/255, blue: 155/255, alpha: 1)
-        ]
-        let randomColor = Int.random(in: 0..<(colors.count))
-        self.view.backgroundColor = colors[randomColor]
-    }
     
     func generateRandomColor() -> UIColor {
         let hue : CGFloat = CGFloat(arc4random() % 256) / 256
